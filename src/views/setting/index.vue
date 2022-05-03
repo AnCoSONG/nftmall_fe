e912b4a03dddb1d87f6c604b5f69554192e066a1
AnCoSONG <1197252996@qq.com>
2022-05-03 16:31:01 +0800

    feat: 🎸 add doc-viewer base

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,9 +1,9 @@
 <template>
 <Subpage title="设置">
     <div class="setting">
-        <CellItem text="用户协议"></CellItem>
-        <CellItem text="关于BSN文昌链"></CellItem>
-        <CellItem text="商务合作"></CellItem>
+        <CellItem text="用户协议" right-icon="arrow" @click=""></CellItem>
+        <CellItem text="关于BSN文昌链" right-icon="arrow"></CellItem>
+        <CellItem text="商务合作" right-icon="arrow"></CellItem>
         <!-- 登录后才显示 -->
         <DangerBtn text="退出登录"/>
     </div>
@@ -13,6 +13,8 @@
 import Subpage from '../../components/Subpage.vue';
 import CellItem from 'comps/CellItem.vue';
 import DangerBtn from '../../components/DangerBtn.vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
 </script>
 <style lang="scss" scoped>
 .setting {
